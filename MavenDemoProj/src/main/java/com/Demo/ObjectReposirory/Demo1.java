96f54f15f9f9708adb5dea21fe05b35010dbe6ad
Sobhana-Reddy <sobhana.reddy93@gmail.com>
2020-09-12 15:03:57 +0530

    test

---
+++ MavenDemoProj/src/main/java/com/Demo/ObjectReposirory/Demo1.java
@@ -0,0 +1,13 @@
+package com.Demo.ObjectReposirory;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("test method is checking...");
+	}
+
+}
